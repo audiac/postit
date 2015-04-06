6cc1dc6d1a99a9f6fb00e57edb46b554fe22c206
Ryan <ryan.geo@gmail.com>
2015-04-06 12:08:58 -0700

    Add user, post, comment, category and post_category models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
+
+  # get '/posts', to: 'posts#index'
+  # get '/posts/:id', to: 'posts#show'
+  # get '/posts/new', to: 'posts#new'
+  # post '/posts', to: 'posts#create'
+  # get '/posts/:id/edit', to: 'posts#edit'
+  # patch '/posts/:id', to: 'posts#update'
+
+  # all of the above routes are created with the following line
+  # only expose the routes that you need. We don't need destroy
+  resources :posts, except: [:destroy] do
+    resources :comments, only: [:create]
+  end
+
 end
