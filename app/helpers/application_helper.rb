c8c759c28104cafc2454c75701d22024797a630e
Ryan <ryan.geo@gmail.com>
2015-04-09 08:12:06 -0700

    Add fix_url and friendly_datetime helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def fix_url(orig_url)
+    orig_url.starts_with?('http://') ? orig_url : "http://#{orig_url}"
+  end
+
+  def friendly_datetime(orig_dt)
+    orig_dt.strftime("%m/%d/%Y %l:%M%P %Z")
+  end
 end
