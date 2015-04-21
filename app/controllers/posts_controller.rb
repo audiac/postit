3eab0c13226017f94b84ad7299359e7fd636d6a1
Ryan <ryan.geo@gmail.com>
2015-04-21 08:20:37 -0700

    Convert post voting to ajax call

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,15 +39,19 @@ class PostsController < ApplicationController
   end
 
   def vote
-    vote = Vote.create(vote: params[:vote], voteable: @post, creator: current_user)
+    @vote = Vote.create(vote: params[:vote], voteable: @post, creator: current_user)
 
-    if vote.valid?
-      flash[:notice] = "Your vote was counted."
-    else
-      flash[:error] = "You can only vote on a post once."
+    respond_to do |format|
+      format.html do
+        if @vote.valid?
+          flash[:notice] = "Your vote was counted."
+        else
+          flash[:error] = "You can only vote on a post once."
+        end
+        redirect_to :back
+      end
+      format.js
     end
-
-    redirect_to :back
   end
 
   private
