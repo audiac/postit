2a72c22f3a1e6f750723d3fff93492497fe1e002
Ryan <ryan.geo@gmail.com>
2015-04-22 07:28:27 -0700

    Add Ajax voting to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,16 +17,20 @@ class CommentsController < ApplicationController
   end
 
   def vote
-    comment = Comment.find(params[:id])
-    vote = Vote.create(vote: params[:vote], voteable: comment, creator: current_user)
+    @comment = Comment.find(params[:id])
+    @vote = Vote.create(vote: params[:vote], voteable: @comment, creator: current_user)
 
-    if vote.valid?
-      flash[:notice] = "Your vote was counted."
-    else
-      flash[:error] = "You can only vote on a comment once."
+    respond_to do |format|
+      format.html do
+        if @vote.valid?
+          flash[:notice] = "Your vote was counted."
+        else
+          flash[:error] = "You can only vote on a comment once."
+        end
+        redirect_to :back
+      end
+      format.js
     end
-
-    redirect_to :back
   end
 
   private
