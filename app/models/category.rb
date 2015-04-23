0e259f1417804d1aac8a3b9403a101c6d8693d6d
Ryan <ryan.geo@gmail.com>
2015-04-22 19:22:09 -0700

    Add slug to categories on name

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,17 @@
 class Category < ActiveRecord::Base
+
+  after_validation :generate_slug
+
   has_many :post_categories
   has_many :posts, through: :post_categories
 
   validates :name, presence: true, length: { minimum: 5 }
+
+  def generate_slug
+    self.slug = self.name.gsub(' ', '-').downcase
+  end
+
+  def to_param
+    self.slug
+  end
 end
