9774c11e0a0dd3ef2cd1c4f61ee58c4cfdefcb33
Ryan <ryan.geo@gmail.com>
2015-04-26 12:13:31 -0700

    Add json endpoints for post index and show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,10 +5,24 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all.sort_by { |post| post.total_votes }.reverse
+
+    respond_to do |format|
+      format.html
+      format.json do
+        render json: @posts
+      end
+    end
   end
 
   def show
     @comment = Comment.new
+
+    respond_to do |format|
+      format.html
+      format.json do
+        render json: @post
+      end
+    end
   end
 
   def new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+
   before_action :set_user, only: [:show, :edit, :update]
   before_action :require_same_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -48,4 +50,5 @@ class UsersController < ApplicationController
         redirect_to root_path
       end
     end
+
 end
